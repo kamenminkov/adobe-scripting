0cb0f1581a92e46422cfde12ee59419d0c0e370c
Kamen Minkov <kamen.minkov@gmail.com>
2022-08-13 21:46:57 +0300

    Temporary solution with hardcoded values

--- watermark-picture.ts
+++ watermark-picture.ts
@@ -3,32 +3,33 @@
 */
 
 const WATERMARK_PATH = "C:\\Users\\Kamen\\Desktop\\kamen.png";
+const WATERMARK_SCALE = 70;
 
 addPictureWatermark();
 
-const { charIDToTypeID, executeAction } = app;
+const { charIDToTypeID, executeAction, stringIDToTypeID } = app;
 
-function addPictureWatermark() {
+function addPictureWatermark(): void {
+	if (!app.documents.length) {
+		return;
+	}
+
 	// app.open(WATERMARK_PATH);
 
-	const doc = app.activeDocument;
-	const layerRef = app.activeDocument.artLayers.add();
-	const layer = app.activeDocument.artLayers.getByName("Layer 1");
+	// const doc = app.activeDocument;
+	// const layerRef = app.activeDocument.artLayers.add();
+	// const layer = app.activeDocument.artLayers.getByName("Layer 1");
 
 	// const selectedFile = new File(WATERMARK_PATH);
 
 	placeWatermark(WATERMARK_PATH);
 }
 
-function placeWatermark(watermarkPath: string) {
-	if (!app.documents.length) {
-		return;
-	}
+function placeWatermark(watermarkPath: string, offset: number = 100): void {
+	const watermarkFile = new File(watermarkPath);
 
-	var fileObj = new File(watermarkPath);
-
-	if (!fileObj.exists) {
-		alert(fileObj.name + " does not exist!");
+	if (!watermarkFile.exists) {
+		alert(watermarkFile.name + " does not exist!");
 
 		return;
 	}
@@ -43,7 +44,22 @@ function placeWatermark(watermarkPath: string) {
 		var layers = app.activeDocument.layers; // get layers
 		app.activeDocument.activeLayer = layers[0]; // Target Top Layer
 
-		placeFile(fileObj); // Place in file the Watermark png file
+		const docHeight = doc.height;
+		const docWidth = doc.width;
+
+		var offsetPosX = (docWidth as number) - 0.5 * offset;
+		var offsetPosY = (docHeight as number) - 0.5 * offset; // - offset);
+
+		// alert(`w: ${docWidth} h: ${docHeight}`);
+
+		const orientation =
+			docWidth > docHeight ? Direction.HORIZONTAL : Direction.VERTICAL;
+
+		placeWatermarkFromFile(
+			watermarkFile,
+			{ x: offsetPosX, y: offsetPosY },
+			orientation
+		); // Place in file the Watermark png file
 	} catch (e) {
 		alert(e + ": on line " + e.line);
 	} finally {
@@ -53,19 +69,44 @@ function placeWatermark(watermarkPath: string) {
 	}
 }
 
-function placeFile(placeFile: string | File) {
-	var desc21 = new ActionDescriptor();
+function placeWatermarkFromFile(
+	watermarkPath: string | File,
+	offsetFromBottom: { x: number; y: number } = { x: 0, y: 0 },
+	picOrientation: Direction
+): void {
+	const desc21 = new ActionDescriptor();
 
-	desc21.putPath(charIDToTypeID("null"), new File(placeFile as string));
+	if (picOrientation === Direction.HORIZONTAL) {
+		offsetFromBottom = { x: -550, y: 250 };
+	} else {
+		offsetFromBottom = { x: -250, y: 600 };
+	}
+
+	desc21.putPath(charIDToTypeID("null"), new File(watermarkPath as string));
 	desc21.putEnumerated(
 		charIDToTypeID("FTcs"),
 		charIDToTypeID("QCSt"),
 		charIDToTypeID("Qcsa")
 	);
 
-	var desc22 = new ActionDescriptor();
-	desc22.putUnitDouble(charIDToTypeID("Hrzn"), charIDToTypeID("#Pxl"), 0.0);
-	desc22.putUnitDouble(charIDToTypeID("Vrtc"), charIDToTypeID("#Pxl"), 0.0);
+	const desc22 = new ActionDescriptor();
+	desc22.putUnitDouble(
+		charIDToTypeID("Hrzn"),
+		charIDToTypeID("#Pxl"),
+		offsetFromBottom.x
+	);
+	desc22.putUnitDouble(
+		charIDToTypeID("Vrtc"),
+		charIDToTypeID("#Pxl"),
+		offsetFromBottom.y
+	);
+
 	desc21.putObject(charIDToTypeID("Ofst"), charIDToTypeID("Ofst"), desc22);
+
+	const idPrc = charIDToTypeID("#Prc");
+
+	desc21.putUnitDouble(charIDToTypeID("Wdth"), idPrc, WATERMARK_SCALE);
+	desc21.putUnitDouble(charIDToTypeID("Hght"), idPrc, WATERMARK_SCALE);
+
 	executeAction(charIDToTypeID("Plc "), desc21, DialogModes.NO);
 }
